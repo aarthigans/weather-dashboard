92ca6110781f70b9ce03c51b37f2be7b230e720d
Lakshmi <mlakshmi.76@gmail.com>
2022-06-16 08:09:38 -0400

    main functionn

--- asset/script.js
+++ asset/script.js
@@ -5,6 +5,98 @@ let saved_cities = []
 const api_key = 'db2e0817e0753a4bbbaf3164c30e16ba'
 const date = moment().format('ddd, DD/MM/YYYY')
 
+
+
+
+function get_req(city_id, lon, lat) {
+
+
+    if (city_id !== null) { //If null passed , use browser geolocation to get coordinates on first run
+        lon = world_cities[city_id].Lng
+        lat = world_cities[city_id].Lat
+    }
+
+    //set URL for One Call API 
+    let queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}&units=imperial`
+    //https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=35&lon=139&appid={API key}
+    //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+    $.ajax({
+        method: 'GET',
+        url: queryURL
+    }).then(function (response) {
+        $('img').show()
+        $('.main').show()
+        if (city_id !== null) {
+            $('#city').text(`${world_cities[city_id].City}, ${world_cities[city_id].Country} - ${date}`)
+        } else {
+
+            let city = response.timezone.split('/')[1] + ', ' + response.timezone.split('/')[0]
+            $('#city').text(`${city} - ${date}`)
+        }
+        //Populate to page the response result
+        $('#city').append($(`<span><img alt="weather image" src="http://openweathermap.org/img/w/${response.current.weather[0].icon}.png" width=80></span>`))
+        $('#temp').html(`Temp: ${response.current.temp}<sup>0</sup>F`)
+        $('#humidity').text(`Humidity: ${response.current.humidity}%`)
+        $('#wind').text(`Wind Speed: ${response.current.wind_speed} m/h`)
+        $('#uv').text(`UV index: `)
+
+        let uv_color = ""
+
+        //UV Index coloring
+        switch (parseInt(response.current.uvi)) {
+            case 0:
+                uv_color = '#00b050'
+                break;
+            case 1:
+                uv_color = '#00b050'
+                break;
+            case 2:
+                uv_color = '#00b050'
+                break;
+            case 3:
+                uv_color = '#ffff00'
+                break;
+            case 4:
+                uv_color = '#ffff00'
+                break;
+            case 5:
+                uv_color = '#ffff00'
+                break;
+            case 6:
+                uv_color = '#ff9933'
+                break;
+            case 7:
+                uv_color = '#ff9933'
+                break
+            case 8:
+                uv_color = '#c00000'
+                break;
+            case 9:
+                uv_color = '#c00000'
+                break;
+            case 10:
+                uv_color = '#c00000'
+                break;
+            default:
+                uv_color = '#d39dd3'
+                break;
+        }
+        let uvi = $(`<span style="background-color:${uv_color}">${response.current.uvi} </span>`)
+        $('#uv').append(uvi)
+
+        //5 Day Forecast 
+        for (let i = 1; i < 6; i++) {
+            $(`h3.day${i}`).text(`${moment().add(i, 'day').format('DD/MM/YY')}`)
+            $(`img.day${i}`).attr('src', `http://openweathermap.org/img/w/${response.daily[i].weather[0].icon}.png`)
+            $(`p.day${i}`).html(`Temp.: ${Math.round(response.daily[i].temp.day)} <sup> o</sup>F<br>Humidity: ${response.daily[i].humidity}%`)
+
+        }
+    }).catch(function (error) {
+        console.error(error);
+    })
+}
+
+
 //Remove city from Saved items
 function remove_city(id) {
     for (let i in saved_cities) {
